3d104d6a65b7b28605859f8c444cf5006202147b
Cronvus <Cronvus@gmail.com>
2024-08-16 22:51:50 +0300

    Add script.js, Two button, new div Ult.

---
+++ script.js
@@ -0,0 +1,26 @@
+let buttonClock = document.querySelector(`.clockButton`);
+let UltClock = document.querySelector('.UltClock');
+let buttonDrill = document.querySelector(`.drillButton`);
+let UltDrill = document.querySelector('.UltDrill');
+
+function digitalClock() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    document.getElementById("id_clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+    setTimeout("digitalClock()", 1000);
+}
+window.onload = digitalClock;
+
+buttonClock.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    UltClock.classList.toggle("hidden");
+})
+buttonDrill.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    UltDrill.classList.toggle("hidden");
+})
