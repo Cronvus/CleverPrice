ff62d06d920b7763aa5af0dacd949cd0ad61126d
maeTX <134536932+maeTX@users.noreply.github.com>
2024-10-25 13:04:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ window.onload = digitalClock;
 
 
 function toggleDiv() {
-    const group1Div = document.getElementsByTagName("group1");
-    const group2Div = document.getElementsByTagName("group2");
+    const group1Div = document.getElementsById("group1");
+    const group2Div = document.getElementsById("group2");
 
     if (group1Div.classList.contains("visible-group")) {
         group1Div.classList.remove("visible-group");
